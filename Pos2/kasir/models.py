f0dc924181407cc6aba75085d95d6a4258922c0c
Ahmed Muzaki <elzackq@gmail.com>
2021-02-09 14:04:10 +0700

    model data barang

--- Pos2/kasir/models.py
+++ Pos2/kasir/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+# Data Barang
+
 class Barang(models.Model):
-    kode = models.ChartField(max_length=255)
-    kategori = models.ChartField(max_length=255)
-    kode_barang = models.ChartField(max_length=255)
-    nama_barang = models.ChartField(max_length=255)
-    harga_beli = models.DecimalField()
-    stok = models.DecimalField()
-    lokasi = models.ChartField
+    kode = models.CharField(max_length=255)
+    kategori = models.CharField(max_length=255)
+    kode_barang = models.CharField(max_length=255)
+    nama_barang = models.CharField(max_length=255)
+    harga_beli = models.DecimalField(max_digits=12, decimal_places=2)
+    harga_jual = models.DecimalField(max_digits=12, decimal_places=2)
+    stok = models.DecimalField(max_digits=4, decimal_places=0)
+    lokasi = models.CharField
     
+# Data Stok
+
+# Data Supplier
+
