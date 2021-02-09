f4c07b9635ec70f71ea6fabe8cdc22a3a69ab345
Ahmed Muzaki <elzackq@gmail.com>
2021-02-09 13:11:36 +0700

    create first pos

--- POS/barang/models.py
+++ POS/barang/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Barang(models.Model):
+    kode_barang = models.CharField(max_length=20)
+    nama_barang = models.CharField(max_length=255)
+    harga_beli = models.()
+    harga_
+
+    def __init__(self):
+        pass
+        
