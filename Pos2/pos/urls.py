5dc810a84a62ef16390dfc6d646a89d275ec61a8
Ahmed Muzaki <elzackq@gmail.com>
2021-02-11 22:29:43 +0700

    create layout buat extends

--- Pos2/pos/urls.py
+++ Pos2/pos/urls.py
@@ -5,9 +5,13 @@ from django.shortcuts import render
 
 def index(rq):
     return render(rq, 'index.html')
+
+def cont(rq):
+    return render(rq, 'contact.html')
     
 urlpatterns = [
     path('', index),
+    path('cont/', cont),
     path('kasir/', include('kasir.urls')),
     path('admin/', admin.site.urls),
 ]
